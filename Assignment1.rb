ad939ffd98e6b4a0cd0a237ab89a7e0e18be5a41
meetesh06 <meeteshmehta4@gmail.com>
2016-08-14 20:53:41 +0530

    Init Repo

---
+++ Assignment1.rb
@@ -0,0 +1,7 @@
+message = " welcome "
+name = "Ruby"
+puts message.strip.capitalize + " " + name.capitalize
+puts message.strip.capitalize + " " + name.upcase
+puts message.strip + name.downcase 
+puts name.reverse + message.capitalize 
+puts message.strip.upcase + " " + name.upcase
